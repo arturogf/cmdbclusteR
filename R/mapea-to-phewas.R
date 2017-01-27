a30cc12968de3a99226cde2ff3bec50f0505cd29
Arturo Gonzalez <arturogf@gmail.com>
2017-01-27 12:56:27 +0100

    - tranlate and slightly reorganized mapea-to-phewas.R script

--- R/mapea-to-phewas.R
+++ R/mapea-to-phewas.R
@@ -1,66 +1,73 @@
-# This script creates a new file with PHEWAS-coded columns from the ICD9 original file
-f="/Users/arturogf/Documents/Unidad\ Innovacion/hyponatremia/pacientes\ -\ 2011\ -\ 2015/siadh-mayor18-transformado.csv"
-mydata=read.csv(f, header=TRUE, sep=";", fileEncoding="UTF-8", as.is = TRUE, check.names = FALSE) 
+# This script creates a new file with PHEWAS-coded columns as mapped from the ICD9-coded original file
 
-# Defina el path al fichero de mapeo phewas
-fphewas="/Users/arturogf/Documents/Unidad\ Innovacion/hyponatremia/pacientes\ -\ 2011\ -\ 2015/Henar/PheWAS_code_translation_v1_2-ORIGINAL.txt"
-# todas las columnas (11) son de tipo character
+# -------------- define path to PHEWAS mapping file --------------
+fphewas="../data/mappings/PheWAS_code_translation_v1_2-ORIGINAL.txt"
+# indicate that all columns (11) are of type character 
 colClasses=c(rep("character",11))
 mapeo=read.csv(fphewas, header=TRUE, sep="\t", colClasses=colClasses,fileEncoding="UTF-8", as.is = TRUE, check.names = FALSE) 
 
-# suponemos que la primera columna con codigos es la siguiente a Dx.Todos
-pos_primer_icd9<-match("Dx.Todos",names(mydata))
-pos_primer_icd9<-pos_primer_icd9 + 1
+# -------------- define file input --------------
+f="/Users/arturogf/Documents/Unidad\ Innovacion/hyponatremia/pacientes\ -\ 2011\ -\ 2015/siadh-mayor18-transformado.csv"
+mydata=read.csv(f, header=TRUE, sep=";", fileEncoding="UTF-8", as.is = TRUE, check.names = FALSE) 
 
-codigos<-colnames(mydata[,pos_primer_icd9:ncol(mydata)])
+# -------------- define file output --------------
+fout="/Users/arturogf/Documents/Unidad\ Innovacion/hyponatremia/pacientes\ -\ 2011\ -\ 2015/siadh-mayor18-transformado-phewas.csv"
 
-phewas_codes<-vector()
+# we suppose that first column with ICD9 separated codes is the following to Dx.Todos
+pos_first_icd9 <- match("Dx.Todos", names(mydata))
+pos_first_icd9 <- pos_first_icd9 + 1
 
-# miramos a que mapean todos los icd9
-for (i in codigos) { 
-	pos<-which(mapeo$icd9==i)
-	if (length(pos)!=0) {
-		newcode<-mapeo[pos,3]
-		# añadimos al vector el código phewas
-		if (!(newcode %in% phewas_codes))
-			phewas_codes<-c(phewas_codes,newcode)
-	}
+# create a vector with the icd9 code strings and an empty vector for phewas codes
+icd9_codes <- colnames(mydata[, pos_first_icd9:ncol(mydata)])
+phewas_codes <- vector()
+
+# for each icd9 code we look up the phewas mapping table (column 3 is the phewas code) 
+for (i in icd9_codes) {
+  pos <- which(mapeo$icd9 == i)
+  if (length(pos) != 0) {
+    newcode <- mapeo[pos, 3]
+    # If not present already, we add the phewas code to the phewas_codes vector
+    if (!(newcode %in% phewas_codes))
+      phewas_codes <- c(phewas_codes, newcode)
+  }
 }
 
 colClasses=c(rep("numeric",length(phewas_codes)))
 
-dataph<-read.table(text = "",col.names = phewas_codes, colClasses=colClasses, as.is = TRUE, check.names = FALSE)
-dataph[1:nrow(mydata),]<-0
-
+# create a new dataframe for the phewas columns and set it to 0 for the whole population
+dataph <- read.table(text = "",col.names = phewas_codes, colClasses=colClasses, as.is = TRUE, check.names = FALSE)
+dataph[1:nrow(mydata),] <- 0
 
 nomapeo<-vector()
 
 for (l in 1:nrow(mydata)) {
-	#print(l)
-	# hacemos un split por el caracter ']'
-	dx_todos_sep<-unlist(strsplit(as.character(mydata[l,match("Dx.Todos",names(mydata))]),'\\]'))
-	for (d in dx_todos_sep) {
-		# quitamos el corchete de inicio 
-		h<-substring(d,2)
-		pos<-which(mapeo$icd9==h)
-		if (length(pos)!=0)
-		 	dataph[l,mapeo[pos,3]]<-1
-		 else 
-		 	if (!(h %in% nomapeo))
-		 		nomapeo<-c(nomapeo,h)
-	}
+  #print(l)
+  # we split the codes field (string) using character ']'.
+  dx_todos_sep <-
+    unlist(strsplit(as.character(mydata[l, match("Dx.Todos", names(mydata))]), '\\]'))
+  for (d in dx_todos_sep) {
+    #  we substring avoiding the initial '['
+    h <- substring(d, 2)
+    pos <- which(mapeo$icd9 == h)
+    # we fill the column for the phewas code as mapped from the icd9 at pos
+    if (length(pos) != 0)
+      dataph[l, mapeo[pos, 3]] <- 1
+    else
+      if (!(h %in% nomapeo))
+        nomapeo <- c(nomapeo, h)
+  }
 }
 
+# the icd9 codes without PheWAS mapping are printed out
 for (i in nomapeo)
-	print(paste("No se encontró mapeo del ICD-9",i,sep=" "))
+  print(paste("INFO: There was no PheWAS mapping for ICD-9 code", i, sep =" "))
 
-# copiamos las primeras columnas
-pos_primer_icd9<-match("Dx.Todos",names(mydata))
-mycopy<-mydata[,c(1:pos_primer_icd9)]
+# we do a copy of the first columns before the icd9 codes 
+pos_first_icd9 <- match("Dx.Todos", names(mydata))
+mycopy <- mydata[, c(1:pos_first_icd9)]
 
-# unimos los dos data frames en uno
-mezcla<-cbind(mycopy,dataph)
+# merge the two data frames into one
+mezcla <- cbind(mycopy,dataph)
 
-# salida a fichero salida de la tabla de estadisticas de prevalentes
-fout="/Users/arturogf/Documents/Unidad\ Innovacion/hyponatremia/pacientes\ -\ 2011\ -\ 2015/siadh-mayor18-transformado-phewas.csv"
+# we write the output of phewas mapping to a defined file
 write.table(mezcla,fout,FALSE,sep=";",row.names = FALSE, fileEncoding = "UTF-8",dec=",")
