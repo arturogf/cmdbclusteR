ca454b887efb56f10171b461dc8994b3e9f8f7bf
IratiPablos <iratipablos@gmail.com>
2017-02-23 12:21:26 +0100

    AsignaCluster modification with: -Remove all at the begining. -Posible input types. -col_patientID is 2, and not 1. -threshold_per_cluster ir 0, because with 15 there are problems. -codes_to_drop is empty and the posssible codes to drop are in coments. -We add a new column to store the number of unique patients of each cluster. -We add the number of patients for each cluster in that column.
    (The directories and the input_type are the originals, we should change them to run the script in our computer)

--- R/asignaclusters.R
+++ R/asignaclusters.R
@@ -1,21 +1,26 @@
 # This script receives a population file with prevalence info 
 # and returns the same file including a column with the cluster assigned
 
+#Removes everything before starting
+rm (list=ls())
+
 # include source files
 source("/Users/arturogf/cmdbclusteR/R/funciones.R")
 source("/Users/arturogf/cmdbclusteR/R/ordiplot.R")
 
+# Input type can be PHEWAS os ICD9-4
 input_type<-"ICD9-4"
+#input_type<-"PHEWAS"
 
 # define column number where patient ID is stored in original mydata
-col_patientID<-1
+col_patientID<-2
 
 # Define num of clusters
 num_clusters <- 30
 # Define num of bootstraping iterations
 num_bootstrap <- 10
 # Define threshold for intra-cluster contribution that I want to show in final output
-threshold_per_cluster <- 15
+threshold_per_cluster <- 0
 
 #siadh with prevalence information
 finput="/Users/arturogf/cmdbclusteR/data/processed/siadh-phewas-prevalencia2.csv"
@@ -28,7 +33,11 @@ pos_first_field <- match("Dx.Todos", names(mydata))+1
 
 # e.g. for SIADH, we can remove, hiponatremia, essential hipertension, diabetes, 
 # hiperlipidemia, hipercolesterolemia, tobacco disorder
-codes_to_drop <- c("276.12", "401.1", "250.2", "272.11", "272.1", "318", "253.7")
+# Posible codes to drop in Hiponatremia SIADH (ICD9 = 253.6)
+#codes_to_drop <- c("276.12", "401.1", "250.2", "272.11", "272.1", "318", "253.7")
+# Posible codes to drop in mood disorder (ICD9 = 296)
+#codes_to_drop <- c("401.9","272.0","250.0")
+codes_to_drop<-""
 
 # We subset the feature matrix, dropping the selected variables to remove and the rows with all columns==0
 parcial <- subsetPhewasVars(mydata, TRUE, codes_to_drop, pos_first_field)
@@ -72,9 +81,9 @@ d2 <- function(X) ade4::dist.binary(X, method = "2")
 # --- here we have to define which clustering operations to perform, including method (ward, single, etc.) ---
 # Carry out the similarity profile analysis with 50 bootstrapping iterations
 res.ward.siadh <-simprof(mij, num.expected=500, num.simulated=499,
-                    method.cluster="ward.D", method.distance=d2,
-                    method.transform="identity", alpha=0.05,
-                    sample.orientation="row",silent=FALSE, increment=10)
+                         method.cluster="ward.D", method.distance=d2,
+                         method.transform="identity", alpha=0.05,
+                         sample.orientation="row",silent=FALSE, increment=10)
 
 library(fpc)
 library(prabclus)
@@ -106,16 +115,9 @@ miplot <- ClusterOrdiPlot(distance_measure, salida$cluster, 0)
 dev.off()
 #salida$cluster<-groups
 
-# ---- if needed, add the patient record number if needed for exploration -----
-salida$NHC <- mydata[row.names(parcial), col_patientID]
-
-# We write the file with individuals and cluster assignment plus patient record number
-write.table(salida,fclusters,FALSE,sep=";",row.names = FALSE, fileEncoding = "UTF-8")
 
 #################################################################################
 
-# we remove the patient record number from last column
-salida <- salida[, -ncol(salida)]
 
 clusters <- list()
 superan <- list()
@@ -130,82 +132,97 @@ for (i in 1:num_clusters) {
   for (j in c(1, 1:ncol(clusters[[i]]))) {
     clusters[[i]][, j] <- as.numeric(clusters[[i]][, j])
   }
-	
+  
   # print the number of records
   print(paste("nrows=", toString(nrow(clusters[[i]]), sep = " ")))
   
-	#clusters[[i]]<-data.frame(salida[salida$cluster==i,],check.names = FALSE)
-	#clusters[[i]]<-sapply(clusters[[i]][clusters[[i]]$cluster==i,],as.integer)
-	
+  #clusters[[i]]<-data.frame(salida[salida$cluster==i,],check.names = FALSE)
+  #clusters[[i]]<-sapply(clusters[[i]][clusters[[i]]$cluster==i,],as.integer)
+  
   # drop the last column with the cluster number and add the Total and Percentage rows at the end
-	clusters[[i]] <- clusters[[i]][, -ncol(clusters[[i]])]
-	clusters[[i]]["Total", 1:ncol(clusters[[i]])] <- colSums(clusters[[i]][, 1:ncol(clusters[[i]])])
-	clusters[[i]]["Porcentaje", 1:ncol(clusters[[i]])] <- clusters[[i]]["Total", 1:ncol(clusters[[i]])] / (nrow(clusters[[i]]) - 1) *100
-	
-	# store the percentages for each cluster for all the phewas codes
-	statsclusters[i + 1, 1:ncol(clusters[[i]])] <-clusters[[i]]["Porcentaje", 1:ncol(clusters[[i]])]
-	
-	# store the number of patients in each cluster
-	statsclusters[i + 1, ncol(clusters[[i]]) + 1] <- nrow(clusters[[i]]) - 2
-	
-	# look which pathologies' presence is over intra-cluster threshold and store its position in superan[[i]]
-	superan[[i]] <- vector()
-	for (j in 1:ncol(clusters[[i]])) {
-	  if (statsclusters[i + 1, j] > threshold_per_cluster)
-	    superan[[i]] <- c(superan[[i]], j)
-	}
-	# give names to superan[[i]] vector elements using the codes from original data 
-	names(superan[[i]]) <- colnames(parcial)[superan[[i]]]
-
-	# update cells of superan[[i]] by replacing stored positions with values 
-	for (x in 1:length(superan[[i]])) {
-	  pos <- superan[[i]][x]
-	  superan[[i]][x] <- statsclusters[i + 1, pos]
-	}
-	
-	# order superan[[i]] co in decreasing order of intra-cluster contribution
-	superan[[i]] <- sort(superan[[i]], decreasing = T)
-	
-	# initialize column that will store description for the codes that are higher than defined threshold
-	statsclusters[i + 1, ncol(clusters[[i]]) + 2] <- ""
-
-	if (input_type=="PHEWAS"){
-	  # we update superan[i] cells again, preceeding them with each phewas code description
-	  for (x in 1:length(superan[[i]])) {
-	    pos <- superan[[i]][x]
-	    superan[[i]][x] <-
-	      paste(mapeo[which(mapeo[["phewas_code"]] == names(superan[[i]][x])), "phewas_string"][1],
-	            as.character(round(as.numeric(superan[[i]][x]), 2)),
-	            sep = " (")
-	    superan[[i]][x] <- paste(superan[[i]][x], ")", sep = "")
-	    
-	    # concatenate each phewas description for all codes that are higher than threshold 
-	    statsclusters[i+1,ncol(clusters[[i]])+2]<-paste(statsclusters[i+1,ncol(clusters[[i]])+2],superan[[i]][x], sep=" | ")
-	  }
-	}
-	else {
-	  # we update superan[i] cells again, preceeding them with each phewas code description
-	  for (x in 1:length(superan[[i]])) {
-	    pos <- superan[[i]][x]
-	    superan[[i]][x] <-
-	      paste(mapeo[which(mapeo[["icd9"]] == names(superan[[i]][x])), "icd9_string"][1],
-	            as.character(round(as.numeric(superan[[i]][x]), 2)),
-	            sep = " (")
-	    superan[[i]][x] <- paste(superan[[i]][x], ")", sep = "")
-	    
-	    # concatenate each phewas description for all codes that are higher than threshold 
-	    statsclusters[i+1,ncol(clusters[[i]])+2]<-paste(statsclusters[i+1,ncol(clusters[[i]])+2],superan[[i]][x], sep=" | ")
-	  }
-	}
+  clusters[[i]] <- clusters[[i]][, -ncol(clusters[[i]])]
+  clusters[[i]]["Total", 1:ncol(clusters[[i]])] <- colSums(clusters[[i]][, 1:ncol(clusters[[i]])])
+  clusters[[i]]["Porcentaje", 1:ncol(clusters[[i]])] <- clusters[[i]]["Total", 1:ncol(clusters[[i]])] / (nrow(clusters[[i]]) - 1) *100
+  
+  # store the percentages for each cluster for all the phewas codes
+  statsclusters[i + 1, 1:ncol(clusters[[i]])] <-clusters[[i]]["Porcentaje", 1:ncol(clusters[[i]])]
+  
+  # store the number of episodes in each cluster
+  statsclusters[i + 1, ncol(clusters[[i]]) + 1] <- nrow(clusters[[i]]) - 2
+  
+  # We create a new colon of 0 to store de numbre of unique patients in each cluster
+  statsclusters[i + 1, ncol(clusters[[i]]) + 2] <- 0
+  
+  # look which pathologies' presence is over intra-cluster threshold and store its position in superan[[i]]
+  superan[[i]] <- vector()
+  for (j in 1:ncol(clusters[[i]])) {
+    if (statsclusters[i + 1, j] > threshold_per_cluster)
+      superan[[i]] <- c(superan[[i]], j)
+  }
+  # give names to superan[[i]] vector elements using the codes from original data 
+  names(superan[[i]]) <- colnames(parcial)[superan[[i]]]
+  
+  # update cells of superan[[i]] by replacing stored positions with values 
+  for (x in 1:length(superan[[i]])) {
+    pos <- superan[[i]][x]
+    superan[[i]][x] <- statsclusters[i + 1, pos]
+  }
+  
+  # order superan[[i]] co in decreasing order of intra-cluster contribution
+  superan[[i]] <- sort(superan[[i]], decreasing = T)
+  
+  # initialize column that will store description for the codes that are higher than defined threshold
+  statsclusters[i + 1, ncol(clusters[[i]]) + 3] <- ""
+  
+  if (input_type=="PHEWAS"){
+    # we update superan[i] cells again, preceeding them with each phewas code description
+    for (x in 1:length(superan[[i]])) {
+      pos <- superan[[i]][x]
+      superan[[i]][x] <-
+        paste(mapeo[which(mapeo[["phewas_code"]] == names(superan[[i]][x])), "phewas_string"][1],
+              as.character(round(as.numeric(superan[[i]][x]), 2)),
+              sep = " (")
+      superan[[i]][x] <- paste(superan[[i]][x], ")", sep = "")
+      
+      # concatenate each phewas description for all codes that are higher than threshold 
+      statsclusters[i+1,ncol(clusters[[i]])+3]<-paste(statsclusters[i+1,ncol(clusters[[i]])+3],superan[[i]][x], sep=" | ")
+    }
+  }
+  else {
+    # we update superan[i] cells again, preceeding them with each phewas code description
+    for (x in 1:length(superan[[i]])) {
+      pos <- superan[[i]][x]
+      superan[[i]][x] <-
+        paste(mapeo[which(mapeo[["icd9"]] == names(superan[[i]][x])), "icd9_string"][1],
+              as.character(round(as.numeric(superan[[i]][x]), 2)),
+              sep = " (")
+      superan[[i]][x] <- paste(superan[[i]][x], ")", sep = "")
+      
+      # concatenate each phewas description for all codes that are higher than threshold 
+      statsclusters[i+1,ncol(clusters[[i]])+3]<-paste(statsclusters[i+1,ncol(clusters[[i]])+3],superan[[i]][x], sep=" | ")
+    }
+  }
 }
 
 # Rellenamos el significado de cada código phewas en la fila 1 de stats
 #for (i in 1:ncol(clusters[[2]]))
-#  statsclusters[1,i]<-mapeo[which(mapeo[["phewas_code"]]==colnames(statsclusters)[i]),"phewas_string"][1]             
+#  statsclusters[1,i]<-mapeo[which(mapeo[["phewas_code"]]==colnames(statsclusters)[i]),"phewas_string"][1] 
+
+# ---- if needed, add the patient record number if needed for exploration -----
+salida$NHC <- mydata[row.names(parcial), col_patientID]
+
+colnames(statsclusters)[ncol(statsclusters)] <- "Intracluster_contribution"
+colnames(statsclusters)[ncol(statsclusters)-1] <- "unique_patients"
+colnames(statsclusters)[ncol(statsclusters)-2] <- "nepisodes"
+
+
+for (i in 1:num_clusters) {
+  # store the number of unique patients in each cluster
+  statsclusters[i + 1, ncol(clusters[[i]]) + 2] <- length(unique(as.data.frame(salida[which(salida$cluster == i), ])$NHC))
+  #print(paste("i: ",statsclusters[i + 1, ncol(clusters[[i]]) + 2]))
+}
+# we remove the patient record number from last column
+#salida <- salida[, -ncol(salida)]            
 
 # We write the file with all the statitics per cluster
 write.table(statsclusters,fstats,FALSE,sep=";",row.names = FALSE, fileEncoding = "UTF-8",dec=",")
-
-
-
-
